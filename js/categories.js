84e3198050aaf21017e8acfca6a69fd8889ad531
PeterKvayt <peter.kvayt@gmail.com>
2020-04-08 22:38:22 +0300

    Начал пеерносить фильмы в films.js

---
+++ js/categories.js
@@ -0,0 +1,4 @@
+var categories = [
+  'drama',
+  'documentary'
+]
---
+++ js/films.js
@@ -0,0 +1,39 @@
+
+let films = [
+  new Film(
+    name = 'Сенна',
+    link = 'https://rezka.ag/films/documentary/5967-senna-2010.html',
+    description = '',
+    category = categories[1]
+  ),
+  new Film(
+    name = 'Богемская рапсодия',
+    link = 'https://rezka.ag/films/drama/28929-bogemskaya-rapsodiya-2018.html',
+    description = '',
+    category = categories[0]
+  ),
+  new Film(
+    name = 'Человек, который познал бесконечность',
+    link = 'https://rezka.ag/films/drama/14116-chelovek-kotoryy-poznal-beskonechnost.html',
+    description = '',
+    category = categories[0]
+  ),
+  new Film(
+    name = 'Социальная сеть',
+    link = 'https://rezka.ag/films/drama/4648-socialnaya-set-2010.html',
+    description = '',
+    category = categories[0]
+  ),
+  new Film(
+    name = 'Король говорит',
+    link = 'https://rezka.ag/films/drama/957-korol-govorit-2010.html',
+    description = '',
+    category = categories[0]
+  ),
+  new Film(
+    name = '',
+    link = '',
+    description = '',
+    category = categories[0]
+  ),
+]
